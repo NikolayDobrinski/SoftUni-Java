cac96080252367849b3d103cf44a937d2e3a401f
Nikolay Dobrinski <nikolay.dobrinski@gmail.com>
2021-06-17 12:50:01 +0300

    Added exam preparation tasks into mySql package.

---
+++ src/softuni/mySql/ExamPrep/The_fastest_player_by_towns_LEFT_JOIN_MAX_WHERE_GROUP_BY.sql
@@ -0,0 +1,44 @@
+SELECT
+    MAX(sd.speed) AS max_speed,
+    t.name AS town_name,
+    s.name AS stadium_name,
+    te.name AS team_name,
+    p.first_name
+FROM
+    towns AS t
+        LEFT JOIN
+    stadiums AS s
+    ON t.id = s.town_id
+        LEFT JOIN
+    teams AS te
+    ON s.id = te.stadium_id
+        LEFT JOIN
+    players AS p
+    ON te.id = p.team_id
+        LEFT JOIN
+    skills_data AS sd
+    ON sd.id = p.skills_data_id
+WHERE te.name != 'Devify'
+GROUP BY t.id
+ORDER BY max_speed DESC, t.name;
+
+SELECT
+    MAX(sd.speed) AS max_speed,
+    t.name AS town_name
+FROM
+    towns AS t
+        LEFT JOIN
+    stadiums AS s
+    ON t.id = s.town_id
+        LEFT JOIN
+    teams AS te
+    ON s.id = te.stadium_id
+        LEFT JOIN
+    players AS p
+    ON te.id = p.team_id
+        LEFT JOIN
+    skills_data AS sd
+    ON sd.id = p.skills_data_id
+WHERE te.name != 'Devify'
+GROUP BY t.id
+ORDER BY max_speed DESC, t.name;
